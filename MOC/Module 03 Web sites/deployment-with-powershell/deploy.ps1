bc0e529ef26b477d257d470605202f1c868e27f2
Jalal Hejazi <jalal@SU.INT>
2017-10-18 13:23:53 +0200

    '.'

---
+++ MOC/Module 03 Web sites/deployment-with-powershell/deploy.ps1
@@ -0,0 +1,86 @@
+﻿<#
+ Deploy Web Apps from my private gitlab repo
+ 
+ 
+AzureCloud set fra en udvikler OOP, en Object Tree. 
+For at oprette/administrerer WebSites som har typen AppService, skal man igennem Root-access først. 
+
+Root-level er API Access 
+https://docs.microsoft.com/en-us/rest/api/
+
+GET https://management.azure.com/subscriptions/{subscriptionId}/resources?api-version=2017-05-10
+
+http GET https://management.azure.com/subscriptions/6ef2cbd4-1b87-4409-ab0e-403ffe9024b3/resources?api-version=2017-05-10
+
+
+Azure API version1 ASM > 
+Azure API version2 ARM > 
+                        Subscription > 
+                            ResourceGroup >
+                                App Service plan>     
+                                        App Service> 
+
+#>
+
+
+
+# Authenticate to ARM
+Login-AzureRmAccount
+
+
+#######################################################################
+## Readonly data
+# Get-AzureRmSubscription
+$Subscription_Azure_pass    = "Azure-pass-MS-532"
+$Subscription_Azure_pass_Id = "a22beb2b-b84a-4811-a34f-8eda7bdaaae5"
+
+# Set the Subscription for deployment 
+Select-AzureRmSubscription  -SubscriptionName   $Subscription_Azure_pass
+Set-AzureRmContext          -SubscriptionId     $Subscription_Azure_pass_Id
+
+#######################################################################
+
+
+
+#######################################################################
+# New Resources to create for first time (run once)
+
+$ResourceGroupName = "ARMResourceGroup" 
+$ARMServicePlan    = "ARMServicePlan"
+$Location          = "South Central US"
+$ResourceName      = "ms20532-lab03-version-x"
+
+# Create resource group (only run once)
+# New-AzureRmResourceGroup -Name ARMResourceGroup -Location $Location -Force
+
+# Create app service plan (only run once)
+# New-AzureRmAppServicePlan -ResourceGroupName $ResourceGroupName -Name $ARMServicePlan -Location $Location -Tier "Standard"  -NumberofWorkers 1 -WorkerSize "Small"
+
+# Migrate a website to the ARMServicePlan
+#$plan = @{'serverfarm' = $ARMServicePlan}
+#Set-AzureRmResource -ResourceName $ResourceName -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/sites' -ApiVersion '2014-04-01' -Plan $plan
+# There used to be Set-AzureResource and Get-AzureResource
+
+# Enable Always On for an API app 
+# Set-AzureRmResource -ResourceName $ResourceName  -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Web/sites' -ApiVersion '2015-08-01' -Properties @{"siteConfig" = @{"AlwaysOn" = $false}} -force 
+
+#######################################################################
+
+
+
+
+#######################################################################
+<#
+    Deploy as many times as needed 
+    psedit '.\azuredeploy.json'
+    psedit '.\azuredeploy.parameters.json'
+#>
+
+
+New-AzureRmResourceGroupDeployment -name "powershellDeployment" `
+-ResourceGroupName $ResourceGroupName `
+-TemplateFile '.\azuredeploy.json' `
+-TemplateParameterFile '.\azuredeploy.parameters.json'
+
+#######################################################################
+
--- MOC/Module 05 Designing Cloud Applications for Resiliency/readme.md
+++ MOC/Module 05 Designing Cloud Applications for Resiliency/readme.md
@@ -8,3 +8,59 @@ script automation for traffic manager
 
 * https://git.itacademy.dk/cloud/azure-quickstart-templates/tree/master/101-traffic-manager-external-endpoint
 
+
+
+##  High Performance ASP.NET 
+
+* Async/Await patter
+
+* State Management 
+
+	* Browser LocalStorage 
+	* HTML5 offline mode
+	* HTML5 ServiceWorker 
+	* HTML5 clientSide JSON databaser 
+	* Redis store for sessions/caching/audit/monitor
+
+* Nodejs is much more Scalable for concurrent user than dotnet 
+
+
+
+
+
+## Cloud patterns
+
+Guidelines and Templates for reuseable solutions. 
+
+"Reuseable means also repeatable"
+
+* Retry pattern at temporary (network-level/unavailability/fault)
+	* more code within the catch-block than try-blok 
+	* Entity Framework version 6 includes built-in retry pattern
+	* Use Observable patterns instead of promise pattern because of retry and cancellation 
+
+* partitioning at data-level [scaling]
+	* more architecture than developer focus
+
+*  Valet-key pattern [Azure Storage account]
+	* return token/id/resource-path/HATEOS instead of BLOBs for client approval. 
+	* The user get access to ressoures using link-pattern 
+
+```
+protected images/binaries can be access using SAS (Shared Access Signature ==> token key)
+
+	* https://ms532storage.blob.core.windows.net/<SAS-Token>
+	* https://ms532storage.file.core.windows.net/<SAS-Token>
+	* https://ms532storage.queue.core.windows.net/<SAS-Token>
+	* https://ms532storage.table.core.windows.net/<SAS-Token>
+
+```
+
+Tools available :
+		* https://azure.microsoft.com/da-dk/features/storage-explorer/
+		* extenstions from Visual Studio: search for Cloud Explorer 
+
+
+
+* Redis Cache
+
