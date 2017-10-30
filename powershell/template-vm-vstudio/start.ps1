bfd7a82e6d4a4a5c6e3e5aebe2c649871f99dd39
dev <dev@superusers.dk>
2017-10-30 14:21:31 +0100

    '.'

---
+++ powershell/template-vm-vstudio/start.ps1
@@ -0,0 +1,7 @@
+ 
+
+ # . ./deploy.ps1 -TemplateParameterFile "env-development.json"
+
+ # . ./deploy.ps1 -TemplateParameterFile "env-production.json"
+
+ . ./deploy.ps1 -TemplateParameterFile "env-development.json"
