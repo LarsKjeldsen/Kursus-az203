c293a5d285715987e55550d44a90a7b8ff6a1064
dev <dev@superusers.dk>
2017-10-30 16:12:54 +0100

    '.'

--- powershell/template-vm-vstudio/start.ps1
+++ powershell/template-vm-vstudio/start.ps1
@@ -1,29 +1,14 @@
  <# 
-
-Powershell environment: 
-    Set-executionpolicy unrestricrted  -force
-    Set-executionpolicy remotesigned   -force
-
-Login-AzureRmAccount  er ikke tilgængeligt
-
-download og installere Azure-ps 
-
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-
-# curl https://get.scoop.sh
-
-scoop install azure-ps
-
- 
 Name     : Azure-pass-MS-532
 Id       : a22beb2b-b84a-4811-a34f-8eda7bdaaae5
 
 #>
 
-
-
 ## I skal lave om på  subscriptionId
-$subscriptionId         = "**************a22beb2b-b84a-4811-a34f-8eda7bdaaae5"
+$subscriptionId         = "*****************"
+
+
+## resten af navngivning er op til dig
 
 $resourceGroupName      = "kursus-ms532-gruppe"
 $resourceGroupLocation  = "north europe" 
