cb3d98c3a08ad2b04fcf6479241bd34da5ea13e2
Jalal Hejazi <jalal@SU.INT>
2017-11-29 11:40:35 +0100

    '.'

---
+++ azure-functions/azure-functions-visual-studio/03/demos/after/03PublishToAzure/LoanApplications/MakeApplication.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace LoanApplications
+{
+    public static class MakeApplication
+    {
+        [FunctionName("MakeApplication")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
+            HttpRequestMessage req, 
+            TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            LoanApplication application = await req.Content.ReadAsAsync<LoanApplication>();
+
+            log.Info($"Application received: {application.Name} {application.Age}");
+
+            // TODO: write to queue
+
+            return req.CreateResponse(HttpStatusCode.OK, $"Loan application submitted for {application.Name}");
+        }
+    }
+}
---
+++ azure-functions/azure-functions-visual-studio/05/demos/before/04EventHubTest/EventHubSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using LoanApplications;
+
+namespace EventHubSender
+{
+    class Program
+    {               
+        static async Task Main(string[] args)
+        {
+            EventHubClient client = CreateEventHubClient();
+
+            var message = new LoanApplication
+            {
+                Name = "ArnoldEventHubTest",
+                Age = 100
+            };
+
+            Console.WriteLine("Press enter to send");
+            Console.ReadLine();
+
+            await client.SendAsync(
+                new EventData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message))));
+
+            Console.WriteLine("Message sent");
+
+            await client.CloseAsync();
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+
+        private static EventHubClient CreateEventHubClient()
+        {
+            const string ConnectionString = 
+                "Endpoint=sb://loanapplicationshubtest.servicebus.windows.net/;SharedAccessKeyName=devtest;SharedAccessKey=ouCNsHGkP8Nj+lqtTMi2tEf3CrCsu6pflspL3iOxkzs=;EntityPath=loan-applications";
+
+            var connectionStringBuilder = 
+                new EventHubsConnectionStringBuilder(ConnectionString);
+
+            return EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+        }
+    }
+}
