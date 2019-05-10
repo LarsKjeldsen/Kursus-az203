836fb4e556145ef3b2c5e9c9db8a45a745bb1fb7
Jalal Hejazi <jalal@superusers.dk>
2019-05-10 12:16:19 +0200

    .

--- cosmosdb/todoApp/config.js
+++ cosmosdb/todoApp/config.js
@@ -5,7 +5,7 @@ config.port = process.env.PORT || '3000'
 // Azure CosmosDB --> account 
 config.host = process.env.HOST || "https://cosmos2020.documents.azure.com:443/"
 
-// Azure cosmodDB --> keys --> PRIMARY KEY
+// Azure cosmodDB --> keys --> Read/Write keys --> PRIMARY KEY
 config.authKey = process.env.AUTH_KEY || "WLj1uXajGxXIQd0l4l2dkNyGnX6dErD8Bbrx0hExr2Sq9GpRt6W6vMSgE5iJKqjXWn0ojlGeGD03MIJj7aPIbQ=="
 
 config.databaseId = "ToDoList"
