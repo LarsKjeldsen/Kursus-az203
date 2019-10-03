ce52b0ad33731a6b3397bfab650b60bf25988911
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-10-03 16:46:32 +0000

    Updated 02_Send_event.ps1

--- Event-Grid/02_Send_event.ps1
+++ Event-Grid/02_Send_event.ps1
@@ -1,11 +1,18 @@
-## Send an event to your custom topic
+## Send an event to your Event Grid Topic
 
+# Resource Group Name
 $RG="az-203"
+# The name of Event Grid Topic
 $topicname="superusers-kursus"
 
+# Query the endpoint from eventgrid topic
 $URL=$(az eventgrid topic show --name $topicname -g $RG --query "endpoint" --output tsv)
+
+# Query the key1 from eventgrid topic
 $primaryKey=$(az eventgrid topic key list --name $topicname -g $RG --query "key1" --output tsv)
 
+
+# Build The Message to be sent to Event Grid
 $eventID = Get-Random 99999
 $eventDate = Get-Date -Format s
 $htbody = @{
@@ -21,6 +28,10 @@ $htbody = @{
     dataVersion="1.0"
 }
 
+# Convert from object to JSON
 $body = "["+(ConvertTo-Json $htbody)+"]"
 
+
+# USE HTTP POST with a valid Token and Body as JSON
 Invoke-WebRequest -Uri $URL -Method POST -Body $body -Headers @{"aeg-sas-key" = $primaryKey}
+
