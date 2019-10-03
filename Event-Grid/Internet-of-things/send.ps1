bcac0460e8c0e0bfc5c2b1b432258b91f0b99305
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-10-03 17:59:14 +0000

    Added file send.ps1 into new folder Internet-of-things

---
+++ Event-Grid/Internet-of-things/send.ps1
@@ -0,0 +1,33 @@
+## Send an event to a WebHook
+
+param(
+  $WebHookAddress = "todo",
+  $sharedKey = "todo",
+  $message = "besked",
+  $subject = "titel",
+  $x = "xxx",
+  $y = "yyy",
+  $z = "zzz" 
+)
+
+# Build The Message to be sent to Event Grid
+$eventID = Get-Random 99999
+$eventDate = Get-Date -Format s
+$htbody = @{
+    id=$eventID
+    eventType=$message
+    subject=$subject    
+    eventTime=$eventDate   
+    data= @{
+        x=$x
+        y=$y
+        z=$z
+    }
+    dataVersion="1.0"
+}
+
+# Convert from object to JSON
+$body = "["+(ConvertTo-Json $htbody)+"]"
+
+# USE HTTP POST with a valid Token and Body as JSON
+Invoke-WebRequest -Uri $WebHookAddress -Method POST -Body $body -Headers @{"aeg-sas-key" = $sharedKey}
