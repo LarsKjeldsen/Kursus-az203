f099b0674b628155d5f6aad2ad20743a7a35b453
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-12-16 12:02:41 +0000

    get secrets from azure key-vaults

---
+++ azure-cli/create_linux_vm/az-vm-linux-create-v2.ps1
@@ -0,0 +1,14 @@
+## How to create function in PowerShell
+function create-vm-linux{
+    param(
+        $rgName="vm2020",
+        $VMName="dev2020",
+        $secret
+    )
+    az group create --location westus --name $rgName
+    az vm create --resource-group $rgName --name $VMName --image Debian --admin-username sysadmin --admin-password $secret --tags "env=training"
+}
+
+## get secret from Azure key-vault 
+$secret = get-azure-secret sysadmin
+create-vm-linux  -secret $secret
